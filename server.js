7b07291977cf479c49b4f93c4aaf9fc56568758e
lfnsyhd <alfansyahadahu@gmail.com>
2025-12-27 19:14:09 +0700

    Convert to Vercel serverless format

--- server.js
+++ server.js
@@ -40,7 +40,5 @@ app.use((err, req, res, next) => {
     res.status(500).json({ error: 'Something went wrong!' });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    console.log(`Health check: http://localhost:${PORT}/health`);
-});
+// Export for Vercel serverless
+export default app;
